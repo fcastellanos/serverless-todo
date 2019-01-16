69f3bdc681822ee26f243a0dcae7ec63b2a24692
Fernando Castellanos <fcastellanos@bandsintown.com>
2019-01-15 16:51:27 -0800

    Add python requrements plugin to fix toknens exchange endpoint, also added travis config

--- lambdas/token_exchange.py
+++ lambdas/token_exchange.py
@@ -2,15 +2,13 @@ import json
 import os
 import jwt
 import time
-from auth_header import AuthHeader
+from lambdas.auth_header import AuthHeader
 
 def handler(event, context):
-    request_body = json.loads(event['body'])
     jwt_secret   = os.environ['todo_jwt_secret']
     auth_header  = AuthHeader(event['headers']['Authorization'])
 
-    print("Auth token => {}".format(auth_header.token))
-
+    # NOTE: We now have access to auth_header.token
     # TODO: Verify token
 
     payload = {
@@ -21,7 +19,7 @@ def handler(event, context):
     token = jwt.encode(payload, jwt_secret, algorithm='HS256')
 
     body = {
-        'token': token,
+        'token': token.decode('ascii'),
         'expires_at': payload['exp']
     }
 
@@ -29,6 +27,8 @@ def handler(event, context):
         'statusCode': 200,
         'body': json.dumps(body)
     }
+
+    return response
 
 def get_expiration_hrs(hrs):
     seconds = hrs * 3600
